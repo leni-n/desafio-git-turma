5004958780dc9d7f8507920de0df8a9495368885
leni-n <bachira.migui@gmail.com>
2026-09-09 17:04:33 -0300

    atividade multiplicação

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ function somar(a, b) {
     return a + b;
 }
 
+function multiplicar(c, d) {
+    return c * d;
+}
+
 console.log("10 + 5 =", somar(10, 5));
+console.log("29 x 34  =", multiplicar(29, 34));
